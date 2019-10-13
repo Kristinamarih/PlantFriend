17d43ffe0d74289b3f7d5916cfa9169e13913b08
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-12 17:08:44 -0700

    create models and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :comments
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :products
+    has_many :comments
+    has_many :commented_products, through: :comments, source: :product
 end
