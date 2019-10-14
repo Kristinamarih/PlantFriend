716658f7f2b484eee08bfe83f2429001dd4c48d6
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-13 17:21:38 -0700

    omniauth gem install

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,21 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
 
+    def google
+        @user = User.find_or_create_by(email: auth['info']['email'] ) do |user|
+            user.username = auth['info']['first_name']
+            user.password = SecureRandom.hex(10)
+        end
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            redirect_to '/'
+        end
+    end
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,23 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+
   delete '/logout' => 'sessions#destroy'
+
+  get '/auth/google_oauth2' => 'sessions#google'
   
+  resources :products do
+    resources :comments, only: [:new, :create, :index]
+  end
   resources :categories
   resources :comments
   resources :users do
     resources :posts, only: [:create, :index]
   end
-  resources :products do
-    resources :comments, only: [:new, :create, :index]
-  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
  
