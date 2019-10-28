6baa3a17cf1c42ee0f9f8b8f57f140cf68a21b57
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-28 11:57:15 -0700

    messages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -11,7 +11,7 @@ class ConversationsController < ApplicationController
         else
             @conversation = Conversation.create!(conversation_params)
         end
-        
+         
         redirect_to conversation_messages_path(@conversation)
     end
 
